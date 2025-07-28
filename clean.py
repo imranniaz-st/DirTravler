54f8108e863ef0610509d84dd64d573ee76120ef
imran Niaz <103443033+imranniaz-st@users.noreply.github.com>
2025-07-28 16:45:53 +0000

    Auto-commit on 2025-07-28 16:45:53

--- clean.py
+++ clean.py
@@ -1,4 +1,12 @@
 import re
 
 input_file = 'nmap_targets.txt'
- 
+
+with open(input_file, 'r+') as f:
+    lines = f.readlines()
+    f.seek(0)
+    for line in lines:
+        # Remove lines containing "/open/tcp//ms-wbt-server//"
+        if not re.search(r'/open/tcp//ms-wbt-server//', line):
+            f.write(line)
+    f.truncate()
